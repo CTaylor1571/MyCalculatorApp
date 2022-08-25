99dcfd7f639a1e0a7be3a8dc03ebaf0fab2b1082
CTaylor1571 <90778109+CTaylor1571@users.noreply.github.com>
2022-08-25 07:57:39 -0500

    Edited UI to consider keyboard pop-up, and got button color change to not crash the app

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     String operation="";
     TextView textView;
+    Button addButton ;
+    Button subtractButton ;
+    Button multiplyButton ;
+    Button divideButton ;
+    Button clearButton;
+    Button calculateButton;
+    int blueyColor, tealColor;
+
+
     public void onCalculate(View view){
         EditText boxNum1 = findViewById(R.id.enteredNumber1);
         EditText boxNum2 = findViewById(R.id.enteredNumber2);
@@ -26,10 +38,16 @@ public class MainActivity extends AppCompatActivity {
                 textView.setText(num1-num2+"");
             }
             else if(operation.equals("multiply")){
-                textView.setText(num1*num2+"");
+                double ans = (Math.round((num1*num2)*1000000000000.0))/1000000000000.0;
+                textView.setText(ans+"");
             }
             else if(operation.equals("divide")){
-                textView.setText(num1/num2+"");
+                if(num2!=0){
+                    double ans = (Math.round(num1/num2*1000000000000.0))/1000000000000.0;
+                    textView.setText(ans+"");
+                }else{
+                    textView.setText("Undefined");
+                }
             }
         }else{
             textView.setText("Select an operation or enter numbers");
@@ -44,24 +62,49 @@ public class MainActivity extends AppCompatActivity {
         boxNum2.setText("");
         textView.setText("Answer will appear here");
         operation = "";
+        resetColors();
     }
 
     public void onAddOpClick(View view){
         operation = "add";
+        resetColors();
+        addButton.setBackgroundTintList(ColorStateList.valueOf(tealColor));
     }
     public void onSubtractOpClick(View view){
         operation = "subtract";
+        resetColors();
+        subtractButton.setBackgroundTintList(ColorStateList.valueOf(tealColor));
     }
     public void onMultiplyOpClick(View view){
         operation = "multiply";
+        resetColors();
+        multiplyButton.setBackgroundTintList(ColorStateList.valueOf(tealColor));
     }
     public void onDivideOpClick(View view){
         operation = "divide";
+        resetColors();
+        divideButton.setBackgroundTintList(ColorStateList.valueOf(tealColor));
+    }
+    public void resetColors(){
+        addButton.setBackgroundTintList(ColorStateList.valueOf(blueyColor));
+        subtractButton.setBackgroundTintList(ColorStateList.valueOf(blueyColor));
+        multiplyButton.setBackgroundTintList(ColorStateList.valueOf(blueyColor));
+        divideButton.setBackgroundTintList(ColorStateList.valueOf(blueyColor));
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        blueyColor = ResourcesCompat.getColor(getResources(), R.color.bluey, null); // Copied this process from a github comment by sat
+        tealColor = ResourcesCompat.getColor(getResources(), R.color.teal_200, null); // https://stackoverflow.com/a/5271452
+
+        addButton = findViewById(R.id.addition);
+        subtractButton = findViewById(R.id.subtraction);
+        multiplyButton = findViewById(R.id.multiplication);
+        divideButton = findViewById(R.id.division);
+        clearButton = findViewById(R.id.clearButton);
+        calculateButton = findViewById(R.id.calculateButton);
     }
 }
